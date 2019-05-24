fd8f7ca8affb35f7115afacc8c3785fe2bb7a775
AUM <47864924+spudnik12@users.noreply.github.com>
2019-05-24 17:25:30 -0400

    Add files via upload

--- hamming.go
+++ hamming.go
@@ -2,37 +2,32 @@
 //and counts the number of point mutations.
 package hamming
 
-import "fmt"
+import "errors"
 
-//Func Distance compares two DNA string values,
+//Distance compares two DNA string values,
 //and returns both an error and an int types value that
 //represents pass/fail length match and point mutation count.
 func Distance(a, b string) (int, error) {
-	//err defaults at nil
-	var err error
 	//Testing for a match in the lengths of string values.
-	//terminates function returning an int type value and an error
 	if len(a) != len(b) {
-		err = fmt.Errorf("")
-		return -1, err
+		return 0, errors.New("mismatching DNA strand lengths")
 	}
 	//Calls on the function CheckPairs to compare point mutations and returns a value.
 	//Calling function Checkpairs to initiate recursion.
-	return CheckPairs(a, b), err
+	return CheckPairs(a, b), nil
 
 }
 
-//Function CheckPairs compares point mutations for matching lengths of DNA,
+//CheckPairs compares point mutations for matching lengths of DNA,
 //and returns the number of occurances.
 func CheckPairs(a, b string) int {
 	if len(a) > 0 {
-		//Testing for point mutations at the first element of the string and returns the sum of all.
-		//Every recursive call  by subtracting the first element from every function call
-		//point mutation
+		//Testing for point mutations at the first element of the string,
+		//and recursion call the rest of the elements.
 		if a[0] != b[0] {
 			return 1 + CheckPairs(a[1:len(a)], b[1:len(b)])
 		}
-		//matching pairs
+
 		if a[0] == b[0] {
 			return 0 + CheckPairs(a[1:len(a)], b[1:len(b)])
 		}

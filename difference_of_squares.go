ac537485ccef0d0e11211763b9a5e14d38dcfcb0
spudnik12 <instant.jello@gmail.com>
2019-09-11 22:54:37 -0400

    Updates with functions; replaced recursive algorithms.

--- difference_of_squares.go
+++ difference_of_squares.go
@@ -1,31 +1,25 @@
 //Package diffsquares determines the square of the sum of n positive integers,
-//the sum of the square of each n positive integer, and the difference between both.
+//the sum of the square of each n positive integer,
+//and the difference between both.
 package diffsquares
 
 //SquareOfSum = (1+2+3+...+n)^2 = n^3 + (n-1)^3 + (n-2)^3 + ... + (n-n)^3.
+//SquareOfSum = n^3 + {n*(n-1)/2}^2
 func SquareOfSum(n int) int {
-	if n == 1 {
-		return n * n * n
-	}
 
-	return n*n*n + SquareOfSum(n-1)
+	return n*n*n + (n*(n-1)/2)*(n*(n-1)/2)
 
 }
 
-//SumOfSquares = (1^2 + 2^2 + ... + n^2) = n^2 + (n-1)^2 + (n-2)^2 + ... + (n-n)^2.
+//SumOfSquares = (1^2 + 2^2 + ... + n^2) = n^2 + (n-1)^2 + (n-2)^2 + ... + (n-n)^2
+// (n/6)*(n+1)*(2n+1)
 func SumOfSquares(n int) int {
 
-	if n == 1 {
-		return n * n
-	}
-	return n*n + SumOfSquares(n-1)
+	return n * (n + 1) * (2*n + 1) / 6
 }
 
-//Difference = SquareOfSum - SumOfSquares = n^2(n-1)+(n-1)^2(n-2)+...+1^2(n-n).
+//Difference = SquareOfSum - SumOfSquares.
 func Difference(n int) int {
 
-	if n == 1 {
-		return n * n * (n - 1)
-	}
-	return n*n*(n-1) + Difference(n-1)
+	return SquareOfSum(n) - SumOfSquares(n)
 }
